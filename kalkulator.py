7dd9f9b4f1bc69ade4f22eb588c9208253c193ee
artiixes <85556178+artiixes@users.noreply.github.com>
2022-04-01 18:54:46 +0200

    kalkulator_update

--- kalkulator.py
+++ kalkulator.py
@@ -1,22 +1,18 @@
 import sys
 import logging
 
-rodzaj = int(input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: '))
+types = int(input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: '))
 p1 = float(input('Podaj pierwszy składnik: '))
 p2 = float(input('Podaj drugi składnik: '))
-if rodzaj == 1:
-    print('Dodaje ' + str(p1) + ' i ' + str(p2))    
-    p = p1 + p2
-    print('Wynik to ' + str(p))
-if rodzaj == 2:
-    print('Odejmuje ' + str(p1) + ' i ' + str(p2))
-    p = p1 - p2
-    print('Wynik to ' + str(p))
-if rodzaj == 3:
-    print('Mnoże ' + str(p1) + ' i ' + str(p2))
-    p = p1 * p2
-    print('Wynik to ' + str(p))
-if rodzaj == 4:
-    print('Dziele ' + str(p1) + ' i ' + str(p2))
-    p = p1 / p2
-    print('Wynik to ' + str(p))
+
+match types:
+    case 1:
+        print (p1 + p2)
+    case 2:
+        print (p1 - p2)
+    case 3:
+        print (p1 * p2)
+    case 4:
+        print (p1 / p2)
+    case _:
+        print("Wrong number!!")
