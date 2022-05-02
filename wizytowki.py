17bb6f793b1e1c32f393f4fbd7a79aa97a4d25b2
artiixes <85556178+artiixes@users.noreply.github.com>
2022-05-02 11:44:39 +0200

    final update 2

--- wizytowki.py
+++ wizytowki.py
@@ -18,6 +18,8 @@ class base_contact(card):
         super().__init__(name, surname, company, position, email)
         self.phone_number = phone_number
         self._name_length = 0
+    def __repr__(self):
+        return f'base_contact("{self.name}", "{self.surname}", "{self.company}", "{self.position}", "{self.email}", "{self.phone_number}")'
 
     def contact(self):
         print("Kontakuję sie z " + str(self.phone_number))
@@ -53,9 +55,20 @@ def create_contacts(type, number):
             companies = fake.company()
             i = business_contact(name = first_name, surname=last_name, email=emails, business_phone_number=business_phone_numbers, position=positions, company=companies)
             empty.append(i)
+        elif type == 'base':
+            first_name = fake.first_name()
+            last_name = fake.last_name()
+            emails = fake.email()
+            phone_numbers = fake.phone_number()
+            positions = fake.job()
+            companies = fake.company()
+            i = base_contact(name = first_name, surname=last_name, email=emails, phone_number=phone_numbers, position=positions, company=companies)
+            empty.append(i)
+            
     print(empty)
 
 create_contacts('business', 5)
+create_contacts('base', 5)
 
-p = business_contact(name="Dawid", surname="Kowalski", email="kowalski@gmail.com", company="af", position="senior", business_phone_number=123456798)
+p = business_contact(name="Dawid", surname="Płosiński", email="dplosinski97@gmail.com", company="af", position="senior", business_phone_number=123456798)
 p.name_length
