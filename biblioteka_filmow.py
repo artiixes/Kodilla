c573792d7fc4dd845bfa489b530ef49e05f677a6
artiixes <85556178+artiixes@users.noreply.github.com>
2022-05-02 21:37:26 +0200

    update

--- biblioteka_filmow.py
+++ biblioteka_filmow.py
@@ -1,3 +1,7 @@
+import random
+from faker import Faker
+fake = Faker()
+
 class library:
     def __init__(self, title, release_date, genre, views):
         self.title = title
@@ -11,7 +15,8 @@ class library:
 class movies(library):
     def __init__(self, *args, **kwargs):
        super().__init__(*args, **kwargs)
-
+    def __repr__(self):
+        return f'movie("{self.title}", "{self.genre}", "{self.release_date}", "{self.views}")'
     def __str__(self):
         return f'{self.title} ({self.release_date})'
 
@@ -20,6 +25,8 @@ class series(library):
         super().__init__(*args, **kwargs)
         self.episode = episode
         self.season = season
+    def __repr__(self):
+        return f'series("{self.title}", "{self.season}", "{self.episode}", "{self.genre}", "{self.release_date}", "{self.views}")'
     def __str__(self):
         if self.episode >= 0 and self.episode < 10:
             if self.season >= 0 and self.season < 10:
@@ -32,12 +39,44 @@ class series(library):
             else:
                 return f'{self.title} S{self.season}E{self.episode}'
 
+
+
+def m_and_s(number):
+    empty = []
+    for i in range(0, number):
+        a = random.randrange(0, 2)
+        if a == 0:
+            type = 'movie'
+        else:
+            type = 'series'       
+        if type == 'movie':
+            genre = ['Action', 'Romanse', 'Sport', 'Horror', 'Thriller', 'Adventure', 'Documentary']
+            a = random.randrange(0, 7)
+            titles = fake.word()
+            release_dates = fake.date()
+            genres = genre[a]
+            viewss = fake.pyint()
+            i = movies(title=titles, release_date=release_dates, genre=genres, views=viewss)
+            empty.append(i)
+        elif type == 'series':
+            genre = ['Action', 'Romanse', 'Sport', 'Horror', 'Thriller', 'Adventure', 'Documentary']
+            a = random.randrange(0, 7)
+            b = random.randrange(0, 100)
+            c = random.randrange(0, 100)
+            titles = fake.word()
+            release_dates = fake.date()
+            genres = genre[a]
+            viewss = fake.pyint()
+            episodes = b
+            seasons = c
+            i = series(title=titles, release_date=release_dates, genre=genres, views=viewss, episode=episodes, season=seasons)
+            empty.append(i)
+    print(empty)
+    for j in empty:
+        print(j)
+
+
+m_and_s(5)
+
 sim = series(title="The Simpsons", release_date=1999, genre="action", views=55 ,season=10, episode=5)
 pulp = movies(title="Pulp Fiction", release_date=1994, genre="Romantic", views=90)
-print(sim)
-print(pulp)
-
-#6. Przechowuje filmy i seriale w jednej liście?
-#Możesz wytłumaczyć?
-
-
